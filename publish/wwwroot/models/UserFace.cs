29fcfa4805ceffe32d8a2aee6c2c11e3cfcd7600
huytv-2025 <huy.tv@smilepms.com>
2025-12-09 16:07:03 +0700

    s

---
+++ publish/wwwroot/models/UserFace.cs
@@ -0,0 +1,10 @@
+﻿namespace FaceID.Models
+{
+    public class UserFace
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string FaceDescriptor { get; set; } = null!; // 128 float, lưu dạng chuỗi
+        public DateTime CreatedAt { get; set; }
+    }
+}
